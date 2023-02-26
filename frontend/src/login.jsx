e73b297baeb4b4b4f43a3fa40f30befe7bdb1ec1
Mohankumar2004 <mohankumar88387@gmail.com>
2023-02-26 22:09:47 +0530

    first commit

---
+++ frontend/src/login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Submit logic here
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={handleEmailChange}
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={handlePasswordChange}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginPage;
